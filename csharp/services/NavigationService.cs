792704bdaf9cd41d265d2350458144ef8e411fa4
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2025-09-29 23:49:41 +0100

    Update NavigationService.cs

--- csharp/services/NavigationService.cs
+++ csharp/services/NavigationService.cs
@@ -2,12 +2,19 @@ using SpiderNavigation.Models;
 
 namespace SpiderNavigation.Services
 {
-    public class NavigationService
+    public static class NavigationService
     {
         public static Spider Navigate((int Width, int Height) wallSize, 
                                     (int X, int Y, string Orientation) spiderPosition, 
                                     string instructions)
         {
+            // Validate spider starts within grid bounds
+            if (spiderPosition.X < 0 || spiderPosition.X > wallSize.Width || 
+                spiderPosition.Y < 0 || spiderPosition.Y > wallSize.Height)
+            {
+                throw new ArgumentException("Spider starting position is outside wall boundaries");
+            }
+
             var spider = new Spider(spiderPosition.X, spiderPosition.Y, spiderPosition.Orientation,
                                   wallSize.Width, wallSize.Height);
             
