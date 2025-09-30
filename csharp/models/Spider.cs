3f302063edf071f38ee6de10ab87bbed1283dbee
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2025-09-30 17:36:34 +0100

    Update Spider.cs

--- csharp/models/Spider.cs
+++ csharp/models/Spider.cs
@@ -5,10 +5,53 @@ namespace SpiderNavigation.Models
         public int X { get; set; }
         public int Y { get; set; }
         public string Orientation { get; set; }
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
 
-        public override string ToString()
+        public Spider(int x, int y, string orientation, int maxX, int maxY)
         {
-            return $"{X} {Y} {Orientation}";
+            X = x;
+            Y = y;
+            Orientation = orientation;
+            MaxX = maxX;
+            MaxY = maxY;
         }
+
+        public void TurnLeft()
+        {
+            Orientation = Orientation switch
+            {
+                "Up" => "Left",
+                "Left" => "Down", 
+                "Down" => "Right",
+                "Right" => "Up",
+                _ => Orientation
+            };
+        }
+
+        public void TurnRight()
+        {
+            Orientation = Orientation switch
+            {
+                "Up" => "Right",
+                "Right" => "Down",
+                "Down" => "Left", 
+                "Left" => "Up",
+                _ => Orientation
+            };
+        }
+
+        public void MoveForward()
+        {
+            switch (Orientation)
+            {
+                case "Up": if (Y < MaxY) Y++; break;
+                case "Right": if (X < MaxX) X++; break;
+                case "Down": if (Y > 0) Y--; break;
+                case "Left": if (X > 0) X--; break;
+            }
+        }
+
+        public string GetFinalPosition() => $"{X} {Y} {Orientation}";
     }
 }
