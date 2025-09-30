155397e0df602b0d1628470c1cc5f1f83f858721
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2025-09-30 15:27:08 +0100

    Update SpiderTests.cs

--- csharp/tests/SpiderTests.cs
+++ csharp/tests/SpiderTests.cs
@@ -1,6 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SpiderNavigation.Models;
-using SpiderNavigation.Services;
 using SpiderNavigation.Utils;
 
 namespace SpiderNavigation.Tests
@@ -9,122 +8,80 @@ namespace SpiderNavigation.Tests
     public class SpiderTests
     {
         [TestMethod]
-        public void TestExampleNavigation()
+        public void TestExampleFromRequirements()
         {
             // Arrange
-            var wallSize = (7, 15);
-            var spiderPosition = (4, 10, "Left");
-            var instructions = "FLFLFRFFLF";
-            
+            var spider = new Spider(4, 10, "Left", 7, 15);
+            var instructions = "FLEEREFLF";
+
             // Act
-            var spider = NavigationService.Navigate(wallSize, spiderPosition, instructions);
-            
+            spider.ExecuteInstructions(instructions);
+            var result = spider.GetFinalPosition();
+
             // Assert
-            Assert.AreEqual("5 7 Right", spider.GetFinalPosition());
+            Assert.AreEqual("5 7 Right", result);
         }
-        
+
         [TestMethod]
-        public void TestTurnOperations()
+        public void TestTurningLeft()
         {
-            // Arrange
-            var spider = new Spider(0, 0, "Up", 5, 5);
-            
-            // Act & Assert - Right turn
-            spider.TurnRight();
-            Assert.AreEqual("Right", spider.Orientation);
-            
-            // Act & Assert - Left turn
-            spider.TurnLeft();
-            Assert.AreEqual("Up", spider.Orientation);
+            var spider = new Spider(0, 0, "Up", 10, 10);
+            spider.ExecuteInstructions("L");
+            Assert.AreEqual("Left", spider.Orientation);
         }
-        
+
         [TestMethod]
-        public void TestMoveOperations()
+        public void TestTurningRight()
         {
-            // Arrange
-            var spider = new Spider(2, 2, "Up", 5, 5);
-            
-            // Act & Assert - Move up
-            spider.MoveForward();
-            Assert.AreEqual(3, spider.Y);
-            Assert.AreEqual(2, spider.X);
-            
-            // Act & Assert - Turn and move right
-            spider.TurnRight();
-            spider.MoveForward();
-            Assert.AreEqual(3, spider.X);
-            Assert.AreEqual(3, spider.Y);
-        }
-        
-        [TestMethod]
-        public void TestBoundaryMovement()
-        {
-            // Arrange - Spider at top right corner facing up and right
-            var spider = new Spider(5, 5, "Up", 5, 5);
-            
-            // Act - Try to move beyond boundaries
-            spider.MoveForward(); // Up - should not move (already at top)
-            spider.TurnRight();
-            spider.MoveForward(); // Right - should not move (already at right edge)
-            
-            // Assert
-            Assert.AreEqual(5, spider.X);
-            Assert.AreEqual(5, spider.Y);
+            var spider = new Spider(0, 0, "Up", 10, 10);
+            spider.ExecuteInstructions("R");
             Assert.AreEqual("Right", spider.Orientation);
         }
-        
+
         [TestMethod]
-        public void TestInputParsing()
+        public void TestMovingForward()
         {
-            // Arrange & Act
-            var wallSize = InputParser.ParseWallSize("7 15");
-            var spiderPos = InputParser.ParseSpiderPosition("4 10 Left");
-            var instructions = InputParser.ParseInstructions("FLFLFRFFLF");
-            
-            // Assert
-            Assert.AreEqual(7, wallSize.Width);
-            Assert.AreEqual(15, wallSize.Height);
-            Assert.AreEqual(4, spiderPos.X);
-            Assert.AreEqual(10, spiderPos.Y);
-            Assert.AreEqual("Left", spiderPos.Orientation);
-            Assert.AreEqual("FLFLFRFFLF", instructions);
+            var spider = new Spider(0, 0, "Up", 10, 10);
+            spider.ExecuteInstructions("F");
+            Assert.AreEqual(0, spider.X);
+            Assert.AreEqual(1, spider.Y);
         }
-        
+
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void TestInvalidWallSizeInput()
+        public void TestBoundaryLimits()
         {
-            InputParser.ParseWallSize("7");
+            var spider = new Spider(0, 0, "Down", 5, 5);
+            spider.ExecuteInstructions("F");
+            Assert.AreEqual(0, spider.X);
+            Assert.AreEqual(0, spider.Y);
         }
-        
+    }
+
+    [TestClass]
+    public class InputParserTests
+    {
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void TestInvalidSpiderPositionInput()
+        public void ParseWallSize_ValidInput_ReturnsCorrectValues()
         {
-            InputParser.ParseSpiderPosition("4 10 InvalidDirection");
+            var (width, height) = InputParser.ParseWallSize("7 15");
+            Assert.AreEqual(7, width);
+            Assert.AreEqual(15, height);
         }
-        
+
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void TestInvalidInstructionsInput()
+        public void ParseSpiderPosition_ValidInput_ReturnsCorrectValues()
         {
-            InputParser.ParseInstructions("FLFX"); // X is invalid
+            var (x, y, orientation) = InputParser.ParseSpiderPosition("4 10 Left");
+            Assert.AreEqual(4, x);
+            Assert.AreEqual(10, y);
+            Assert.AreEqual("Left", orientation);
         }
-        
+
         [TestMethod]
-        public void TestCompleteCycleTurns()
+        public void ParseInstructions_ValidInput_ReturnsCorrectValue()
         {
-            // Arrange
-            var spider = new Spider(0, 0, "Up", 5, 5);
-            
-            // Act - Complete right turn cycle
-            spider.TurnRight(); // Up -> Right
-            spider.TurnRight(); // Right -> Down
-            spider.TurnRight(); // Down -> Left
-            spider.TurnRight(); // Left -> Up
-            
-            // Assert - Back to original orientation
-            Assert.AreEqual("Up", spider.Orientation);
+            var instructions = InputParser.ParseInstructions("FLEEREFLF");
+            Assert.AreEqual("FLEEREFLF", instructions);
         }
     }
 }
