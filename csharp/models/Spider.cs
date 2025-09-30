4b3437e9a37e82e890c60524cb8d7bac5843fd82
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2025-09-30 16:37:34 +0100

    Update Spider.cs

--- csharp/models/Spider.cs
+++ csharp/models/Spider.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-
 namespace SpiderNavigation.Models
 {
     public class Spider
@@ -8,98 +5,10 @@ namespace SpiderNavigation.Models
         public int X { get; set; }
         public int Y { get; set; }
         public string Orientation { get; set; }
-        public int GridWidth { get; set; }
-        public int GridHeight { get; set; }
-        public List<Position> Path { get; set; }
 
-        public Spider(int x, int y, string orientation, int gridWidth, int gridHeight)
-        {
-            X = x;
-            Y = y;
-            Orientation = orientation;
-            GridWidth = gridWidth;
-            GridHeight = gridHeight;
-            Path = new List<Position> { new Position { X = x, Y = y, Orientation = orientation } };
-        }
-
-        public void TurnLeft()
-        {
-            Orientation = Orientation switch
-            {
-                "Up" => "Left",
-                "Left" => "Down",
-                "Down" => "Right",
-                "Right" => "Up",
-                _ => Orientation
-            };
-            Path.Add(new Position { X = X, Y = Y, Orientation = Orientation });
-        }
-
-        public void TurnRight()
-        {
-            Orientation = Orientation switch
-            {
-                "Up" => "Right",
-                "Right" => "Down",
-                "Down" => "Left",
-                "Left" => "Up",
-                _ => Orientation
-            };
-            Path.Add(new Position { X = X, Y = Y, Orientation = Orientation });
-        }
-
-        public void MoveForward()
-        {
-            switch (Orientation)
-            {
-                case "Up":
-                    if (Y < GridHeight) Y += 1;
-                    break;
-                case "Right":
-                    if (X < GridWidth) X += 1;
-                    break;
-                case "Down":
-                    if (Y > 0) Y -= 1;
-                    break;
-                case "Left":
-                    if (X > 0) X -= 1;
-                    break;
-            }
-            Path.Add(new Position { X = X, Y = Y, Orientation = Orientation });
-        }
-
-        public void ExecuteInstructions(string instructions)
-        {
-            foreach (var instruction in instructions)
-            {
-                switch (instruction)
-                {
-                    case 'L':
-                        TurnLeft();
-                        break;
-                    case 'R':
-                        TurnRight();
-                        break;
-                    case 'F':
-                        MoveForward();
-                        break;
-                    default:
-                        Console.WriteLine($"Unknown instruction: {instruction}");
-                        break;
-                }
-            }
-        }
-
-        public string GetFinalPosition()
+        public override string ToString()
         {
             return $"{X} {Y} {Orientation}";
         }
-    }
-
-    public class Position
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public string Orientation { get; set; } = "";
     }
 }
