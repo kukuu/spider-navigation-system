444c3afc7cd1bd7807be9e0beaa30940aa88c86f
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2025-09-30 16:38:09 +0100

    Update NavigationService.cs

--- csharp/services/NavigationService.cs
+++ csharp/services/NavigationService.cs
@@ -2,24 +2,95 @@ using SpiderNavigation.Models;
 
 namespace SpiderNavigation.Services
 {
-    public static class NavigationService
+    public class NavigationService
     {
-        public static Spider Navigate((int Width, int Height) wallSize, 
-                                    (int X, int Y, string Orientation) spiderPosition, 
-                                    string instructions)
+        private readonly int _maxX;
+        private readonly int _maxY;
+
+        public NavigationService(int maxX, int maxY)
         {
-            // Validate spider starts within grid bounds
-            if (spiderPosition.X < 0 || spiderPosition.X > wallSize.Width || 
-                spiderPosition.Y < 0 || spiderPosition.Y > wallSize.Height)
+            _maxX = maxX;
+            _maxY = maxY;
+        }
+
+        public Spider Navigate(Spider spider, string instructions)
+        {
+            // Create a copy to avoid modifying the original
+            var currentSpider = new Spider 
+            { 
+                X = spider.X, 
+                Y = spider.Y, 
+                Orientation = spider.Orientation 
+            };
+
+            foreach (var instruction in instructions)
             {
-                throw new ArgumentException("Spider starting position is outside wall boundaries");
+                ProcessInstruction(currentSpider, instruction);
             }
 
-            var spider = new Spider(spiderPosition.X, spiderPosition.Y, spiderPosition.Orientation,
-                                  wallSize.Width, wallSize.Height);
-            
-            spider.ExecuteInstructions(instructions);
-            return spider;
+            return currentSpider;
+        }
+
+        private void ProcessInstruction(Spider spider, char instruction)
+        {
+            switch (instruction)
+            {
+                case 'L':
+                    TurnLeft(spider);
+                    break;
+                case 'R':
+                    TurnRight(spider);
+                    break;
+                case 'F':
+                    MoveForward(spider);
+                    break;
+                // Ignore any other characters (like 'E')
+                default:
+                    break;
+            }
+        }
+
+        private void TurnLeft(Spider spider)
+        {
+            spider.Orientation = spider.Orientation switch
+            {
+                "Up" => "Left",
+                "Left" => "Down", 
+                "Down" => "Right",
+                "Right" => "Up",
+                _ => spider.Orientation
+            };
+        }
+
+        private void TurnRight(Spider spider)
+        {
+            spider.Orientation = spider.Orientation switch
+            {
+                "Up" => "Right",
+                "Right" => "Down",
+                "Down" => "Left",
+                "Left" => "Up",
+                _ => spider.Orientation
+            };
+        }
+
+        private void MoveForward(Spider spider)
+        {
+            switch (spider.Orientation)
+            {
+                case "Up":
+                    if (spider.Y < _maxY) spider.Y++;
+                    break;
+                case "Right":
+                    if (spider.X < _maxX) spider.X++;
+                    break;
+                case "Down":
+                    if (spider.Y > 0) spider.Y--;
+                    break;
+                case "Left":
+                    if (spider.X > 0) spider.X--;
+                    break;
+            }
         }
     }
 }
